e9940532726405aba35183171cefe91814f81653
Gabriel Laroche Borba <105392375+glarocheborba@users.noreply.github.com>
2023-04-19 00:33:34 -0300

    Update querys.sql

--- querys.sql
+++ querys.sql
@@ -7,15 +7,31 @@ FROM USUARIO U INNER JOIN ACESSA A
     	ON A.COD_AGEND = AG.COD_AGEND
 WHERE AG.DATA_INICIO = '02-JAN-22' AND U.MATRICULA IN (
     	SELECT A.MATRICULA
-            FROM ALUNO A)
+            FROM ALUNO A);
 
 -- Mostre os usuarios que levaram mais de um 1 visitante 
 SELECT U.NOME, COUNT(*)
 FROM USUARIO U INNER JOIN VISITANTE V ON U.MATRICULA = V.MATRICULA
 GROUP BY U.NOME
-HAVING COUNT(*) > 1 
+HAVING COUNT(*) > 1;
 
 -- Mostre o Num_Sala dos laboratorios que nunca tiveram um agendamento
 SELECT L.NUM_SALA 
 FROM LABORATORIO L LEFT JOIN ACESSA A ON L.COD_LAB = A.COD_LAB INNER JOIN AGENDAMENTO AG ON A.COD_AGEND = AG.COD_AGEND
 WHERE A.COD_LAB IS NULL AND AG.COD_AGEND IS NULL; 
+
+-- Mostre o nome e email dos usuários que ja levaram algum visitante, não pode fazer a consulta com JOIN
+SELECT U.NOME, U.EMAIL
+FROM USUARIO U 
+WHERE EXISTS (
+	SELECT * 
+    FROM VISITANTE V 
+    WHERE V.MATRICULA = U.MATRICULA);
+
+-- Mostre o nome e email dos usuários que nunca levaram algum visitante, não pode fazer a consulta com JOIN
+SELECT U.NOME, U.EMAIL
+FROM USUARIO U 
+WHERE NOT EXISTS (
+	SELECT * 
+    FROM VISITANTE V 
+    WHERE V.MATRICULA = U.MATRICULA);
