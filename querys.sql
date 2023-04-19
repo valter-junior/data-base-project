c9bc303470947a2aecde67aa560dee153cf4dff3
Gabriel Laroche Borba <105392375+glarocheborba@users.noreply.github.com>
2023-04-19 02:54:53 -0300

    Update querys.sql

--- querys.sql
+++ querys.sql
@@ -49,7 +49,14 @@ WHERE P.QTD >= (SELECT AVG(QTD)
     FROM PECA)
     
 -- Mostre a matricula dos alunos que cursam a mesma cadeira e estão no mesmo período do aluno com matricula
-
+SELECT A.MATRICULA 
+FROM ALUNO A
+WHERE (A.CADEIRA, A.PERIODO) = (
+    SELECT A1.CADEIRA, A1.PERIODO
+    FROM ALUNO A1
+    WHERE A1.MATRICULA = 12350
+    AND A.MATRICULA <> 12350 
+);
     
 -- Mostre a matricula dos funcionários que começaram e terminaram uma manutenção no mesmo dia do funcionário com matricula 12356 
 SELECT M1.MATRICULA
